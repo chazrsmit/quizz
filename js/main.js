fef5bb97aed6ba51074f8949a3c660cece8534e4
Charlotte <smit-charlotte@outlook.com>
2025-03-13 10:23:09 +0100

    update

--- js/main.js
+++ js/main.js
@@ -8,17 +8,28 @@ let espace = document.createElement("div")
 intro.appendChild(espace)
 espace.style.marginBottom = "20px"
 
-let next = document.createElement("div")
-intro.appendChild(next)
+// espace contenant les questions et les boutons
+let containerSuivant = document.createElement("div")
+containerSuivant.style.marginTop = "20px"
+intro.appendChild(containerSuivant)
 
+// question 2 + boutons
+let question2 = document.createElement("div")
+containerSuivant.appendChild(question2)
+question2.style.marginBottom = "10px"
+question2.style.display= "none"
+question2.innerText=`2. Question 2`
+
+// Question 1 + boutons 
 let question1 = document.createElement("div")
-intro.appendChild(question1)
+containerSuivant.appendChild(question1)
 question1.innerText=`1. Quelle est la planète la plus grande du système solaire ?`
 question1.style.marginBottom = "10px"
 
+
 let btn1 = document.createElement("button")
 btn1.textContent="Mars"
-intro.appendChild(btn1)
+containerSuivant.appendChild(btn1)
 btn1.classList.add("monBouton")
 btn1.style.marginRight = "10px"
 
@@ -27,18 +38,39 @@ btn1.addEventListener("click", ()=>{
     btn1.remove()
     btn2.remove()
     btn3.remove()
-    next.innerText= "Mauvaise réponse !"
+    question2.style.display="block"
 })
 
 let btn2 = document.createElement("button")
 btn2.textContent="Jupiter"
-intro.appendChild(btn2)
+containerSuivant.appendChild(btn2)
 btn2.classList.add("monBouton")
 btn2.style.marginRight ="10px"
 
+btn2.addEventListener("click", ()=>{
+    question1.remove()
+    btn1.remove()
+    btn2.remove()
+    btn3.remove()
+    score += 1
+    question2.style.display="block"
+})
+
 let btn3 = document.createElement("button")
 btn3.textContent="Saturne"
-intro.appendChild(btn3)
+containerSuivant.appendChild(btn3)
 btn3.classList.add("monBouton")
+
+btn3.addEventListener("click", ()=>{
+    question1.remove()
+    btn1.remove()
+    btn2.remove()
+    btn3.remove()
+    question2.style.display="block"
+})
+
+
+
+
 
 
