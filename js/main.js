a1d7b0ee490701c73fae88424ce503f3255b6308
Charlotte <smit-charlotte@outlook.com>
2025-03-13 09:45:05 +0100

    update

--- js/main.js
+++ js/main.js
@@ -2,3 +2,11 @@ let intro = document.querySelector(".mon-texte")
 let prenom = prompt("Bienvenue ! Quel est ton prénom ?")
 
 intro.innerText = `Bienvenue ${prenom}, te voilà dans ce quizz très facile sur l'astronomie.`
+
+
+let question1 = document.createElement("div")
+intro.appendChild(question1)
+question1.innerText="question 1"
+
+
+
