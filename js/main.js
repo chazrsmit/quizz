da6f6b620279b224d518ec8ebbf9119e2d593d64
Charlotte <smit-charlotte@outlook.com>
2025-03-13 10:00:55 +0100

    update

--- js/main.js
+++ js/main.js
@@ -3,10 +3,23 @@ let prenom = prompt("Bienvenue ! Quel est ton prénom ?")
 
 intro.innerText = `Bienvenue ${prenom}, te voilà dans ce quizz très facile sur l'astronomie.`
 
+let espace = document.createElement("div")
+intro.appendChild(espace)
+espace.style.marginBottom = "20px"
 
 let question1 = document.createElement("div")
 intro.appendChild(question1)
-question1.innerText="question 1"
+question1.innerText=`1. Quelle est la planète la plus grande du système solaire ?`
 
+let btn1 = document.createElement("button")
+btn1.textContent="Mars"
+intro.appendChild(btn1)
+btn1.classList.add("monBouton")
+btn1.style.marginRight = "10px"
+
+let btn2 = document.createElement("button")
+btn2.textContent="Jupiter"
+intro.appendChild(btn2)
+btn2.classList.add("monBouton")
 
 
